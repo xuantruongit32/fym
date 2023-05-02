c8bd5b47e7cf75b71ff0650c5b6425fe268afdee
xuantruongit32 <xuantruongit32@gmail.com>
2023-05-02 09:10:45 +0700

    Create showComboAccount method

--- src/main/java/com/fym/gui/addTransaction.java
+++ src/main/java/com/fym/gui/addTransaction.java
@@ -1,7 +1,8 @@
 
 package com.fym.gui;
-import com.fym.*;
-import java.util.List;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.UUID;
 import com.fym.model.*;
 
 
@@ -18,6 +19,7 @@ public class addTransaction extends javax.swing.JFrame {
     public addTransaction(BudgetManager budgetManager) {
         initComponents();
         showComboCategory(budgetManager);
+        showComboAccount(budgetManager);
     }
     
 
@@ -190,42 +192,18 @@ public class addTransaction extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(addTransaction.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(addTransaction.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(addTransaction.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(addTransaction.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                
-                new addTransaction().setVisible(true);
-            }
-        });
-    }
     private void showComboCategory(BudgetManager budgetManager){
-        List<String> data = budgetManager.categories;
+        HashSet<String> data = budgetManager.getCategories();
         for(String c: data){
             category.addItem(c);
+        }
+        
+    }
+    private void showComboAccount(BudgetManager budgetManager){
+        HashMap<UUID,Account> data = budgetManager.getAccounts();
+        for(Account c: data.values()){
+            account.addItem(c.getName());
         }
         
     }
