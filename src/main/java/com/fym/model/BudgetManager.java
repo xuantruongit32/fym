b4d4378445b7342ce78bb6bfa47ee356f2b3fe75
xuantruongit32 <xuantruongit32@gmail.com>
2023-05-02 21:50:56 +0700

    Overload totalExpenseYearly method

--- src/main/java/com/fym/model/BudgetManager.java
+++ src/main/java/com/fym/model/BudgetManager.java
@@ -486,7 +486,28 @@ public class BudgetManager{
                 if(daysBetween >= 0 && daysBetween <=364){
                     totalExpense+=transaction.getAmount();
                 }
-
+    }
+            return totalExpense;
+}
+         public float totalExpenseYearly(LocalDate date, Account account){  
+            List<Transaction> expenseList = transactions.get("Expense");
+            float totalExpense = 0;
+            for (Transaction transaction : expenseList) {
+                long daysBetween = ChronoUnit.DAYS.between(date,transaction.getDateTime());
+                if(daysBetween >= 0 && daysBetween <=364 && account.equals(transaction.getAccount())){
+                    totalExpense+=transaction.getAmount();
+                }
+    }
+            return totalExpense;
+}
+         public float totalExpenseYearly(LocalDate date, String category){  
+            List<Transaction> expenseList = transactions.get("Expense");
+            float totalExpense = 0;
+            for (Transaction transaction : expenseList) {
+                long daysBetween = ChronoUnit.DAYS.between(date,transaction.getDateTime());
+                if(daysBetween >= 0 && daysBetween <=364 && category.equals(transaction.getCategory())){
+                    totalExpense+=transaction.getAmount();
+                }
     }
             return totalExpense;
 }
